7dd4d45e2ff48b8b04cbdcfef3da3c5996a436a6
Volker Mische <volker.mische@gmail.com>
2020-01-26 13:55:31 +0100

    feat: randomize list
    Currently it's just a fixed seed, but in the future it will be based on
    URL parameters.

--- pretalx_community_voting/views.py
+++ pretalx_community_voting/views.py
@@ -1,3 +1,5 @@
+import random
+
 from django.views.generic.list import ListView
 
 from pretalx.submission.models import Submission
@@ -5,3 +7,10 @@ from pretalx.submission.models import Submission
 
 class SubmissionListView(ListView):
     model = Submission
+    template_name = 'submission/submission_list.html'
+
+    def get_queryset(self):
+        submissions = list(Submission.objects.all())
+        random.seed('abcd')
+        random.shuffle(submissions)
+        return submissions
